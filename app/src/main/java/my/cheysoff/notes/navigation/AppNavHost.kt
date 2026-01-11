91f95b9758e0599e632d13dd7a7b7de2efcbb8de
CHEYSOFF <66472023+CHEYSOFF@users.noreply.github.com>
2026-01-11 19:30:15 +0300

    add navigation

---
+++ app/src/main/java/my/cheysoff/notes/navigation/AppNavHost.kt
@@ -0,0 +1,56 @@
+package my.cheysoff.notes.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import my.cheysoff.feature_auth.ui.AuthEvent
+import my.cheysoff.feature_auth.ui.AuthScreen
+import my.cheysoff.feature_auth.ui.AuthViewModel
+import my.cheysoff.feature_notes.ui.NotesListScreen
+import my.cheysoff.feature_notes.ui.NotesViewModel
+
+@Composable
+fun AppNavHost(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(navController, startDestination = "auth") {
+        // Auth feature
+        composable("auth") {
+            val viewModel: AuthViewModel = hiltViewModel()
+            val state by viewModel.state.collectAsState()
+
+            LaunchedEffect(viewModel) {
+                viewModel.events.collect { event ->
+                    when (event) {
+                        AuthEvent.NavigationToNotesList -> {
+                            navController.navigate("notes") {
+                                popUpTo("auth") { inclusive = true }
+                            }
+                        }
+                    }
+                }
+            }
+
+            AuthScreen(
+                state = state,
+                onIntentReceived = { intent ->
+                    viewModel.processIntent(intent)
+                }
+            )
+        }
+
+        // Notes feature
+        composable("notes") {
+            val viewModel: NotesViewModel = hiltViewModel()
+            val state by viewModel.state.collectAsState()
+
+            NotesListScreen(state = state)
+        }
+    }
+}
