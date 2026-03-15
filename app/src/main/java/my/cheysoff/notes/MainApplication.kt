91a4d6a5598ebe7bbe7de3a0a90b625c739f47b9
CHEYSOFF <66472023+CHEYSOFF@users.noreply.github.com>
2026-03-15 19:31:20 +0300

    review fixes

--- app/src/main/java/my/cheysoff/notes/MainApplication.kt
+++ app/src/main/java/my/cheysoff/notes/MainApplication.kt
@@ -1,12 +1,17 @@
 package my.cheysoff.notes
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        System.loadLibrary("sqlcipher")
+        try {
+            System.loadLibrary("sqlcipher")
+        } catch (e: UnsatisfiedLinkError) {
+            Log.e("MainApplication", "Failed to load sqlcipher library", e)
+        }
     }
 }
